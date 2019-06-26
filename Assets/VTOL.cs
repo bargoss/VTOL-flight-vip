875cd9821abc85345716fb2da5b4466f79d581f0
bargoss <baranyesiltepe98@gmail.com>
2019-06-27 02:20:32 +0300

    initial

---
+++ Assets/VTOL.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VTOL : MonoBehaviour
+{
+    float _verticalThrust;
+    float _forwardThrust;
+
+    public float VerticalThrust
+    {
+        get
+        {
+            return _verticalThrust;
+        }
+        set
+        {
+            _verticalThrust = Mathf.Clamp(value, -1, 1);
+        }
+    }
+    public float ForwardThrust
+    {
+        get
+        {
+            return _forwardThrust;
+        }
+        set
+        {
+            _forwardThrust = Mathf.Clamp(value, -1, 1);
+        }
+    }
+
+    public float Pitch { get => _pitch; set => _pitch = Mathf.Clamp( value,-1,1); }
+    public float Yaw { get => _yaw; set => _yaw = Mathf.Clamp(value, -1, 1); }
+    public float Roll { get => _roll; set => _roll = Mathf.Clamp(value, -1, 1); }
+
+    float _pitch; // mouse up, down
+    float _yaw; // A,D
+    float _roll; // mouse right,left;
+
+    public float verticalForce;
+    public float forwardForce;
+    public float pitchForce;
+    public float yawForce;
+    public float rollForce;
+
+
+    Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ExecuteInputs(Time.deltaTime);
+    }
+
+    void ExecuteInputs(float deltaTime)
+    {
+        Vector3 force = verticalForce * VerticalThrust * transform.up + forwardForce * ForwardThrust * transform.forward;
+        Vector3 torque = pitchForce * Pitch * transform.right + yawForce * Yaw * transform.up + rollForce * Roll * transform.forward;
+
+        rigidbody.AddForce(force * deltaTime * 60);
+        rigidbody.AddTorque(torque * deltaTime * 60);
+    }
+
+    void SmoothInputs()
+    {
+
+    }
+}
---
+++ Assets/VTOLPlayerControl.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VTOLPlayerControl : MonoBehaviour
+{
+    public Camera camera;
+    Vector3 camRod;
+    Vector3 follower;
+    float followDistance = 10;
+
+    VTOL vtol;
+
+    float verticalThrust;
+    float forwardThrust;
+
+    float pitch; // mouse up, down
+    float yaw; // A,D
+    float roll; // mouse right,left;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vtol = GetComponent<VTOL>();
+        follower = transform.position - transform.forward * 20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalThrust = Input.GetAxis("Vertical");
+        forwardThrust = Input.GetAxis("Forward");
+
+        pitch = Input.GetAxis("Mouse Y");
+        yaw = Input.GetAxis("Horizontal");
+        roll = -Input.GetAxis("Mouse X");
+
+        vtol.VerticalThrust = verticalThrust;
+        vtol.ForwardThrust = forwardThrust;
+
+        vtol.Pitch = pitch;
+        vtol.Yaw = yaw;
+        vtol.Roll = roll;
+
+        CameraUpdate();
+    }
+    
+    void CameraUpdate2()
+    {
+        follower.y = transform.position.y;
+        Vector3 toFollower = follower - transform.position;
+        Vector3 followerTargetPos = transform.position + Vector3.ClampMagnitude(toFollower, followDistance);
+
+        follower = followerTargetPos;
+        //follower = Vector3.Lerp(follower, followerTargetPos, Time.deltaTime);
+
+        camera.transform.position = follower + Vector3.up * 3;
+        camera.transform.LookAt(transform.position + transform.forward * 3);
+    }
+    void CameraUpdate()
+    {
+        Vector3 camrod = transform.forward * -5;
+        camrod.y = 0;
+        camera.transform.position = transform.position + camrod + Vector3.up * 2;
+        camera.transform.LookAt(transform.position + transform.forward * 3);
+    }
+}
